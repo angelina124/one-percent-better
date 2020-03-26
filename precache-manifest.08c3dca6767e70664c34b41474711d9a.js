9246ba82f58dbc182f6c8a6d0cf3bd313973ed63
Angelina Saliling <ajs649@cornell.edu>
2020-03-26 14:43:42 -0400

    Updates

---
+++ precache-manifest.08c3dca6767e70664c34b41474711d9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a3daed0a605a97cf9dc356e4ce44b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af4dff68f3f6120b185c",
+    "url": "/static/css/main.66b2066b.chunk.css"
+  },
+  {
+    "revision": "a20954296a9b6bd534ff",
+    "url": "/static/js/2.0af99277.chunk.js"
+  },
+  {
+    "revision": "5ae3353a2b2be5a7881014434722f360",
+    "url": "/static/js/2.0af99277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4dff68f3f6120b185c",
+    "url": "/static/js/main.e7ae17aa.chunk.js"
+  },
+  {
+    "revision": "b9e87cefd57e96bd7138",
+    "url": "/static/js/runtime-main.b1226b7c.js"
+  },
+  {
+    "revision": "fa872aa711f12692397ace00e3670f70",
+    "url": "/static/media/homebg.fa872aa7.jpg"
+  }
+]);
